4ef498202e8cfe0b85b3678889cde03a7df7d68f
Krish Masand <krishmasand@gmail.com>
2016-01-14 11:45:35 -0800

    Twitch Support

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,3 +1,6 @@
+var twitch = false;
+var url = window.location.href;
+twitch = (url.indexOf("twitch.tv") > -1);
 hideTimes();
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -10,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 }, true);
 
 function hideTimes(){
+	if(!twitch){
 		var times = document.getElementsByClassName("video-time")
 		for(i = 0; i < times.length; i++){
 			var vidTime = times[i];
@@ -24,6 +28,12 @@ function hideTimes(){
 		 	par1.appendChild(repText);
 			i--;
 		}
+		times = document.getElementsByClassName("ytp-progress-bar-container")
+		for(i = 0; i < times.length; i++){
+			var vidTime = times[i];
+			vidTime.textContent="Time Hidden by Anticipation for YouTube and Twitch - Use Arrow Keys to seek";
+			vidTime.style.textAlign = "center";
+		}
 		times = document.getElementsByClassName("timestamp")
 		for(i = 0; i < times.length; i++){
 			var vidTime = times[i];
@@ -33,10 +43,23 @@ function hideTimes(){
 		 	par1.appendChild(repText);
 			i--;
 		}
-		times = document.getElementsByClassName("ytp-progress-bar-container")
+	}
+	else{
+		var times = document.getElementsByClassName("player-seek__time player-seek__time--total js-seek-totaltime")
 		for(i = 0; i < times.length; i++){
 			var vidTime = times[i];
-			var par1 = vidTime.parentNode.removeChild(vidTime);
-			i--;
+			vidTime.textContent="Time Hidden by Anticipation for YouTube and Twitch";
 		}
+		var times = document.getElementsByClassName("player-slider player-slider--roundhandle js-seek-slider ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all")
+		for(i = 0; i < times.length; i++){
+			var vidTime = times[i];
+			vidTime.textContent="Time Hidden by Anticipation for YouTube and Twitch - Use Arrow Keys to seek";
+			vidTime.style.textAlign = "center";
+		}
+		var times = document.getElementsByClassName("overlay_info length")
+		for(i = 0; i < times.length; i++){
+			var vidTime = times[i];
+			vidTime.textContent="Time Hidden by Anticipation";
+		}
+	}
 }
